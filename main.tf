fc087f0e7e81b87ce08d988c4785180ac5461ef7
sik2 <bickwang123@gmail.com>
2025-02-06 17:29:59 +0900

    add s3

--- main.tf
+++ main.tf
@@ -217,3 +217,71 @@ resource "aws_instance" "ec2_1" {
 ${local.ec2_user_data_base}
 EOF
 }
+
+# S3 설정 시작
+resource "aws_s3_bucket" "bucket_1" {
+  bucket = "${var.prefix}-bucket-${var.nickname}-1"
+
+  tags = {
+    Name = "${var.prefix}-bucket-${var.nickname}-1"
+  }
+}
+
+data "aws_iam_policy_document" "bucket_1_policy_1_statement" {
+  statement {
+    sid    = "PublicReadGetObject"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket_1.arn}/*"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_1_policy_1" {
+  bucket = aws_s3_bucket.bucket_1.id
+
+  policy = data.aws_iam_policy_document.bucket_1_policy_1_statement.json
+
+  depends_on = [aws_s3_bucket_public_access_block.bucket_1_public_access_block_1]
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_1_public_access_block_1" {
+  bucket = aws_s3_bucket.bucket_1.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket" "bucket_2" {
+  bucket = "${var.prefix}-bucket-${var.nickname}-2"
+
+  tags = {
+    Name = "${var.prefix}-bucket-${var.nickname}-2"
+  }
+}
+
+resource "aws_s3_object" "object_1" {
+  bucket       = aws_s3_bucket.bucket_1.id
+  key          = "/index.html"
+  content      = "Hello"  # 직접 문자열 사용
+  content_type = "text/html"
+  etag       = md5("Hello")
+  depends_on = [aws_s3_bucket.bucket_2]
+}
+
+resource "aws_s3_object" "object_2" {
+  bucket       = aws_s3_bucket.bucket_2.id
+  key          = "/index.html"
+  content      = "Hello"  # 직접 문자열 사용
+  content_type = "text/html"
+  etag       = md5("Hello")
+  depends_on = [aws_s3_bucket.bucket_2]
+}
+# S3 설정 끝
